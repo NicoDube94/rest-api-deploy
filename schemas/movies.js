f6649aaa73161de9e879a8822db5d4c23a7f3e5a
Nico Dube <nicomas.09@gmail.com>
2024-05-11 19:12:34 -0300

    este es un commit mas

---
+++ schemas/movies.js
@@ -0,0 +1,30 @@
+const z=require("zod")
+
+const movieEsc =z.object({
+    title: z.string({
+        invalid_type_error:'película en modo string',
+        required_error:'El titulo de película es requerido'
+    }),
+    year:z.number().int().min(1900).max(2024),
+    director:z.string(),
+    duration:z.number().int().positive(),
+    rate:z.number().min(0).max(10).default(5.5),
+    poster:z.string().url({
+        message:'url invalida'
+    }),
+    genre: z.array(
+        z.enum(['Action','Adventure','Comedy','Drama','Fantasy','Horror','Thriller','Sci-Fi','Romance','Crime'])
+    )
+})
+
+function validarPelicula(object){
+    return movieEsc.safeParse(object);
+}
+
+function validacionparcial(object){
+    return movieEsc.partial().safeParse(object);
+}
+module.exports={
+    validarPelicula,
+    validacionparcial
+}
